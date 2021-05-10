8b14248f5fb427ba5476f4d9f0ef247d79880061
Michael Bartl <michael.bartl@create.at>
2021-05-10 12:09:30 +0200

    change volume encoder rotation

--- keyboards/kyria/keymaps/astwys/keymap.c
+++ keyboards/kyria/keymaps/astwys/keymap.c
@@ -210,9 +210,9 @@ void encoder_update_user(uint8_t index, bool clockwise) {
     else if (index == 1) {
         // Volume control
         if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
             tap_code(KC_VOLD);
+        } else {
+            tap_code(KC_VOLU);
         }
     }
 }
